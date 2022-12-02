9dc7fa70197830a0b2de24145a94364c6f72cdb9
Carlo Tacchella <tacchella.carlo@gmail.com>
2022-12-02 17:45:38 +0100

    Update main.py

--- lezione05/esercizio02_alg_amicizia/gruppo 03/main.py
+++ lezione05/esercizio02_alg_amicizia/gruppo 03/main.py
@@ -1,14 +1,17 @@
+
 def chiamare():
+    
     input("Componi il numero di telefono")  
-    x=input('Sei a casa?')
+    x=input("Sei a casa?")
     if x=='no': 
         input('Gli lascio un messaggio')
         print("Aspetta di essere richiamato")
-    x=input=("Ti va di mangiare qualcosa assieme?")
+    x=input("Ti va di mangiare qualcosa assieme?")
     if x=="no":
         offertabevanda()
-    else:print("Mangiamo qualcosa assieme")
-
+    else:
+        print("Mangiamo qualcosa assieme")
+    
 def offertabevanda():
     y=input("bevi qualcosa di caldo? ")
     if y=="si":
