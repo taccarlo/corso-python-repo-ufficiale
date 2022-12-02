b86a53f8909bd919a6df915d9221f76cd2d729cd
Carlo Tacchella <tacchella.carlo@gmail.com>
2022-12-02 16:23:51 +0100

    Update algoritmo.py

--- lezione05/esercizio02_alg_amicizia/gruppo 06/algoritmo.py
+++ lezione05/esercizio02_alg_amicizia/gruppo 06/algoritmo.py
@@ -12,7 +12,6 @@ def incipit():
         if(risposta == "si"):
             print("Mangiate qualcosa assieme")
             print("Siete diventati migliori amici! Ora hai una persona in più a cui poter rompere le palle in caso di bisogno e viceversa.")
-    
    
 def offertaBevanda():
     print("offerta_bevanda")
@@ -33,19 +32,18 @@ def offertaBevanda():
 def offertaAttivita():
     print("Allora svaghiamoci un po'...")
     attivita = ""
+    tipiace = "no"
     for x in range(6):
         attivita = input("Cos'altro ti va di fare?")
         tipiace = input("è una cosa che ti va di fare anche a te?")
         if(tipiace=="si"):
             print("e facciamolo dai...")
-            print("svagatevi un po' insieme")
-            return
-
+            break
+    if(tipiace != "si"):
         print("Scegli fra tutte le opzioni quella che ti sembra la meno disumana")
         print("fattela piacere")
-        print("svagatevi un po' insieme")
-        print('"Siete diventati migliori amici! Ora hai una persona in più a cui poter rompere le palle in caso di bisogno e viceversa."')
-
+    print("svagatevi un po' insieme")
+    print('"Siete diventati migliori amici! Ora hai una persona in più a cui poter rompere le palle in caso di bisogno e viceversa."')
 
 incipit()
 offertaBevanda()
