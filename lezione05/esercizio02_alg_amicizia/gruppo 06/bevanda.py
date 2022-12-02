5b5943ad98b7eecaf347a4c34c6d038ceb313586
mattiachecchinato <119517007+mattiachecchinato@users.noreply.github.com>
2022-12-02 15:59:24 +0100

    .

--- lezione05/esercizio02_alg_amicizia/gruppo 06/bevanda.py
+++ lezione05/esercizio02_alg_amicizia/gruppo 06/bevanda.py
@@ -1,6 +1,6 @@
 print("E di bere qualcosa di caldo?")
-    risposta = input("si/no: ")
-     if(risposta == "si"):
+risposta = input("si/no: ")
+if(risposta == "si"):
             scelta = input("Preferisci tè, caffè o cioccolata? ")
             if(scelta == "tè"):
                 print("Fatevi sto tè")
@@ -10,4 +10,4 @@ print("E di bere qualcosa di caldo?")
                 print("Fatevi sta cioccolata")
             print("Siete diventati migliori amici! Ora hai una persona in più a cui poter rompere le palle in caso di bisogno e viceversa.")
        
-elif risposta == "no"):
+elif (risposta == "no"):
