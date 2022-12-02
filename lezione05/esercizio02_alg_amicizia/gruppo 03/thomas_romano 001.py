2ae437716ed1cbda6296f17ac111b9e7219999c8
DanielePeruzzi97 <peruzzi.daniele868@gmail.com>
2022-12-02 16:59:25 +0100

    Daniele modified Thomas function

--- lezione05/esercizio02_alg_amicizia/gruppo 03/thomas_romano 001.py
+++ lezione05/esercizio02_alg_amicizia/gruppo 03/thomas_romano 001.py
@@ -1,27 +1,14 @@
-def chiamare(amico):
-    print('componi il numero di telefono')
-    x=(input('sei a casa?'))
-    if x=='sì':
-    
-        x= True
-        x=(input('ti va di mangiare qualcosa?'))
-        if x =='sì':
-            x=True
-        print(chiamare(True))   
-    else:
-        x=False
-        print('gli lascio un messaggio')
-      
-        x=(input('controlliamo se mi ha richiamato'))
-        if x == 'sì':
-            x=True
-            print('ascolto la risposta')
-        else:
-            x=False
-        print('ok')
-    return(True)
-    print('siete diventati migliori amici')
-print(chiamare(True))
+def chiamare():
+    input("Componi il numero di telefono")  
+    x=input('Sei a casa?')
+    if x=='no': 
+        input('Gli lascio un messaggio')
+        print("Aspetta di essere richiamato")
+    x=input=("Ti va di mangiare qualcosa assieme?")
+    if x=="no":
+        offertabevanda()
+    else:print("Mangiamo qualcosa assieme")
+    return
 
 
 
