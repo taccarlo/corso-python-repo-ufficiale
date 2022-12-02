30deafdaef2dd2706831fa79e1f82c7e631073e6
tommiITS <tommasobendinelli2@gmail.com>
2022-12-02 14:42:04 +0100

    zio pera

---
+++ lezione05/esercizio01/zio pera.py
@@ -0,0 +1,7 @@
+def funz (n1,n2,n3):
+    ris=(n1+n2+n3)/3
+    return ris
+n1=int(input("INSERISCI UN NUMERO:"))
+n2=int(input("INSERISCI UN NUMERO:"))
+n3=int(input("INSERISCI UN NUMERO:"))
+print ("RISULTATONE:", funz(n1,n2,n3))
