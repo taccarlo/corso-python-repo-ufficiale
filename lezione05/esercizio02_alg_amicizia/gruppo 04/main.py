2eb28648918b37f769a451ff9161997759637544
N1M3Xx <119055821+N1M3Xx@users.noreply.github.com>
2022-12-02 15:19:43 +0100

    Update main.py

--- lezione05/esercizio02_alg_amicizia/gruppo 04/main.py
+++ lezione05/esercizio02_alg_amicizia/gruppo 04/main.py
@@ -1,12 +1,19 @@
-def incipit():
-    print("funzione 1")
-    return True
-def offertaBevanda():
-    print("funzione 2")
-    return True
 def offertaAttivita():
     print("funzione 3")
     return
+def offertaBevanda():
+    print("funzione 2")
+    return True
+def incipit():
+    numero=("\nComponi il numero di telefono: ")
+    risposta=("\nE' a casa? ")
+    print("\nLasci un messaggio e aspetti di essere richiamato...")
+    risposta=("\nTi va di mangiare qualcosa assieme? ")
+    if(risposta=="si"):
+        print("\nMangiate qualcosa assieme")
+    elif(risposta=="no"):
+        offertaBevanda()
+    return True
 incipit()
-offertaBevanda()
 offertaAttivita()
+print("SIETE DIVENTATI AMICI!!")
