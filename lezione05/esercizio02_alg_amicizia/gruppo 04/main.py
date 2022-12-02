bb4eeedbcd91c056c2890ca5d6e9d5e79b00279b
N1M3Xx <119055821+N1M3Xx@users.noreply.github.com>
2022-12-02 15:05:30 +0100

    Create main.py

---
+++ lezione05/esercizio02_alg_amicizia/gruppo 04/main.py
@@ -0,0 +1,12 @@
+def incipit():
+    print("funzione 1")
+    return True
+def offertaBevanda():
+    print("funzione 2")
+    return True
+def offertaAttivita():
+    print("funzione 3")
+    return
+incipit()
+offertaBevanda()
+offertaAttivita()
