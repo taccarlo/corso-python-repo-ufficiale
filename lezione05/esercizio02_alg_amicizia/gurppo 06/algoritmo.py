e7824ce2140e9cc371ad3eda2e2893857c67c927
illRoncc <roncari13@gmail.com>
2022-12-02 15:18:40 +0100

    uptade _funzione2

---
+++ lezione05/esercizio02_alg_amicizia/gurppo 06/algoritmo.py
@@ -0,0 +1,14 @@
+def incipit():
+    print("funzione 1")
+    return True
+def offertaBevanda():
+    print("offerta_bevanda")
+    return True
+def offertaAttivita():
+    print("funzione 3")
+    return
+incipit()
+offertaBevanda()
+offertaAttivita()
+
+
