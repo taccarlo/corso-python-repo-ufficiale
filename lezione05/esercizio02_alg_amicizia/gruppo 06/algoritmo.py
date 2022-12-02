f9b68d546895c68aeca9b27bc1d70999048a3170
illRoncc <roncari13@gmail.com>
2022-12-02 15:53:19 +0100

    create_bevanda

--- lezione05/esercizio02_alg_amicizia/gruppo 06/algoritmo.py
+++ lezione05/esercizio02_alg_amicizia/gruppo 06/algoritmo.py
@@ -1,9 +1,24 @@
+
 def incipit():
     print("funzione 1")
     return True
+   
 def offertaBevanda():
     print("offerta_bevanda")
-    return True
+    print("E di bere qualcosa di caldo?")
+    risposta = input("si/no: ")
+    if(risposta == "si"):
+            scelta = input("Preferisci te, caffe o cioccolata? ")
+            if(scelta == "tè"):
+                print("Fatevi sto tè")
+            elif(scelta == "caffè"):
+                print("Fatevi sto caffè")
+            elif(scelta == "cioccolata"):
+                print("Fatevi sta cioccolata")
+            print("Siete diventati migliori amici! Ora hai una persona in più a cui poter rompere le palle in caso di bisogno e viceversa.")
+    if(risposta == 'no'):
+        offertaAttivita()
+
 def offertaAttivita():
     print("Allora svaghiamoci un po'...")
     attivita = ""
@@ -14,9 +29,12 @@ def offertaAttivita():
             print("e facciamolo dai...")
             print("svagatevi un po' insieme")
             return
-    print("Scegli fra tutte le opzioni quella che ti sembra la meno disumana")
-    print("fattela piacere")
-    print("svagatevi un po' insieme")
+
+        print("Scegli fra tutte le opzioni quella che ti sembra la meno disumana")
+        print("fattela piacere")
+        print("svagatevi un po' insieme")
+        print('"Siete diventati migliori amici! Ora hai una persona in più a cui poter rompere le palle in caso di bisogno e viceversa."')
+
 
 incipit()
 offertaBevanda()
