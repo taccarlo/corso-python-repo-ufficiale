1aec996a79f3c0b4daac2880aefcfbfee6e81a68
Carlo Tacchella <tacchella.carlo@gmail.com>
2022-12-02 15:02:00 +0100

    Create algoritmo.py

---
+++ lezione05/esercizio02_alg_amicizia/gruppo 00/algoritmo.py
@@ -0,0 +1,14 @@
+def incipit():
+    print("funzione 1")
+    return True
+def offertaBevanda():
+    print("funzione 2")
+    return True
+def offertaAttivita():
+    print("funzione 3")
+    return
+incipit()
+offertaBevanda()
+offertaAttivita()
+
+
