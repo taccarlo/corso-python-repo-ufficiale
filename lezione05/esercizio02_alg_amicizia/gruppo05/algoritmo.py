99c8484097e9fa970534b65e4f49c42dd0d62ccc
tome <tometom0120@gmail.com>
2022-12-02 15:24:00 +0100

    Update algoritmo.py

--- lezione05/esercizio02_alg_amicizia/gruppo05/algoritmo.py
+++ lezione05/esercizio02_alg_amicizia/gruppo05/algoritmo.py
@@ -1,11 +1,11 @@
 def incipit():
-    print("funzione 1 modificata")
+    print("funzione 1")
     return True
 def offertaBevanda():
-    print("funzione 2 modificata")
+    print("funzione 2")
     return True
 def offertaAttivita():
-    print("funzione 3 modificata")
+    print("funzione 3")
     return
 incipit()
 offertaBevanda()
