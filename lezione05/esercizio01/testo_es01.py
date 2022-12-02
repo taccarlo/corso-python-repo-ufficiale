c165489fb85ede4abc3e966b5af59b07ff21aeea
Nebur0 <ruben.poggiani01@libero.it>
2022-12-02 16:39:07 +0100

    bevanda

---
+++ lezione05/esercizio01/testo_es01.py
@@ -0,0 +1,13 @@
+def incipit():
+    print('Funzione 1')
+    return True
+def offertaBevanda():
+    print('Funzionen 2')
+    return True
+def offertaAttivita():
+    print('Funzione 3')
+    return True
+
+incipit()
+offertaBevanda()
+offertaAttivita()
