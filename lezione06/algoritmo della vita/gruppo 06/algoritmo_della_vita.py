c81df0f7370b657f7bf78a671beaa86cb6cf157e
mattiachecchinato <119517007+mattiachecchinato@users.noreply.github.com>
2022-12-06 10:42:47 +0100

    update file

--- lezione06/algoritmo della vita/gruppo 06/algoritmo_della_vita.py
+++ lezione06/algoritmo della vita/gruppo 06/algoritmo_della_vita.py
@@ -4,8 +4,13 @@ def prima_parte():
 
     print("La persona è nata!")
     print("La persona segue le elementari")
-    if(numero_anno==5):
-        seconda_parte()
-    else:
-        n_bocciature = n_bocciature+1
-        if()
+    while((numero_anno<5) and (n_bocciature<3)):
+        print(numero_anno+1, "° delle superiori")
+        risposta=print("Sei stato bocciato?")
+        if(risposta=="si"):
+            n_bocciature = n_bocciature+1
+        else:
+            numero_anno = numero_anno+1
+    seconda_parte()
+        
+        
