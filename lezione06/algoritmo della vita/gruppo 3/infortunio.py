b545d509d11d4b70a9889a495624de1ab57373e9
Thomromano30 <119420083+Thomromano30@users.noreply.github.com>
2022-12-06 10:09:04 +0100

    file infortunio completato

---
+++ lezione06/algoritmo della vita/gruppo 3/infortunio.py
@@ -0,0 +1,30 @@
+#infortunio
+
+
+
+
+def infortunio(persona):
+
+    counter_anni =0
+    
+    while counter_anni <=30:
+    
+        print(counter_anni)
+    
+        counter_anni += 1
+    
+    import random
+
+    casuale = random.randrange(1,60)
+
+    if casuale <=30:
+        print('hai la pensione')
+    else:
+        print('nessun infortunio')
+print(infortunio('persona'))
+
+
+
+
+
+
