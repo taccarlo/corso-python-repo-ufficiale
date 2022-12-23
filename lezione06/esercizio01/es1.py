7c5d01e410c4ff9998f9830dc123fc33ba57617c
hecrini <hasnafattah17@gmail.com>
2022-12-23 15:49:58 +0100

    Create es1.py

---
+++ lezione06/esercizio01/es1.py
@@ -0,0 +1,21 @@
+n=0
+nome = input("inserisci nome:")
+#cognome = input("inserisci cognome")
+"""
+while (n<100):
+    n += 1
+    nome_completo = print(nome +" "+ cognome)
+
+
+numero = 0
+numero = numero + 1
+while (numero < 20):
+    print (numero)
+Chiedere all'utente un numero da inserire, se questo è divisibile per 3 stampa per 10 volte il tuo nome, altrimenti stampa 10 volte la stringa “Non divisibile per 3” ( Consiglio: per conoscere se un numero è divisibile per un altro utilizzare l’operatore resto ‘%’ )
+
+"""
+num = int(input("inserisci un numero: "))
+if (num%3 == 0):
+    while n<10:
+        print(nome+"\n")
+        n +=1
