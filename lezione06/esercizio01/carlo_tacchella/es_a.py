4d0106df18d267d610aaf67edce135bf1789e7f2
Carlo Tacchella <tacchella.carlo@gmail.com>
2022-12-23 15:21:43 +0100

    [UP] excercises Carlo Tacchella

---
+++ lezione06/esercizio01/carlo_tacchella/es_a.py
@@ -0,0 +1,9 @@
+# Stampare a terminale per cento volte 
+# il vostro nome e il vostro cognome
+
+def funzione_01():
+    x = range(1, 100)
+    for n in x:
+        print("Carlo Tacchella", n) 
+
+funzione_01()
