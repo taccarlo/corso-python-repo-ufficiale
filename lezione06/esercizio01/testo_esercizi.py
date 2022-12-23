d2eedf3982c1576206dfa7e69b9b18ed0596d045
Tscippo02 <gabrielesegala02@gmail.com>
2022-12-23 15:12:06 +0100

    Update testo_esercizi.py

--- lezione06/esercizio01/testo_esercizi.py
+++ lezione06/esercizio01/testo_esercizi.py
@@ -9,9 +9,41 @@ Stampare a terminale tutti i numeri pari da 1 a 20 e dopo tutti i numeri dispari
 Chiedere all'utente un numero da inserire, se questo è divisibile per 3 stampa per 10 volte il tuo nome, altrimenti stampa 10 volte la stringa “Non divisibile per 3” ( Consiglio: per conoscere se un numero è divisibile per un altro utilizzare l’operatore resto ‘%’ )
 
 Creare un programma che continua a chiedere all'utente una password finché l'utente non la indovina
-'''
 
+1
 for i in range (100):
     i+=1
     print("Gabriele Segala")
-    
+
+2
+for i in range (20):
+    i+=1
+    print(i)
+
+3
+i=0
+while(i<20):
+    i=i+2
+    print(i)
+
+i=1
+while(i<20):
+    print(i)
+    i=i+2
+
+4
+num = int(input("inserici un numero: "))
+
+if num%3==0:
+    for i in range (10):
+        print("Gabriele Segala")
+        i+=1
+else:
+    for i in range (10):
+        print("non è divisibile per 3")
+        i+=1
+
+'''  
+
+password == "ciaoney"
+
