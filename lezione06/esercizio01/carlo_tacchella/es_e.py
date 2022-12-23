363db61a05d7bc6d3e8389a4115f989759d360cb
N1M3Xx <119055821+N1M3Xx@users.noreply.github.com>
2022-12-23 15:48:04 +0100

    .

--- lezione06/esercizio01/carlo_tacchella/es_e.py
+++
@@ -1,7 +0,0 @@
-# Creare un programma che continua a chiedere 
-# all'utente una password finché l'utente non 
-# la indovina
-pwd = "pantera"
-user_input = input("inserisci la pwd:")
-while(user_input!="pantera"):
-    user_input = input("inserisci nuovamente la pwd:")
