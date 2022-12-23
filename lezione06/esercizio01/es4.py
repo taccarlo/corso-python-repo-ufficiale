b2c9ee45b9131f707f0eb6dcfcf0daa2da1940d4
illRoncc <roncari13@gmail.com>
2022-12-23 15:11:37 +0100

    esercizi svolti

---
+++ lezione06/esercizio01/es4.py
@@ -0,0 +1,10 @@
+#Chiedere all'utente un numero da inserire, se questo è divisibile per 3 stampa per 10 volte il tuo nome, 
+# altrimenti stampa 10 volte la stringa “Non divisibile per 3” 
+# Chiedi all'utente di inserire un numero
+numero = int(input("Inserisci un numero: "))
+if numero % 3 == 0:
+  for i in range(10):
+    print("Alessandro")
+else:
+  for i in range(10):
+    print("Non divisibile per 3")
