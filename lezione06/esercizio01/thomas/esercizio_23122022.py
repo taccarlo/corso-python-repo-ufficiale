5a6bdf7c1068eef09b9cba998fd633e300af0a13
Thomromano30 <119420083+Thomromano30@users.noreply.github.com>
2022-12-23 15:43:57 +0100

    Create esercizio_23122022.py

---
+++ lezione06/esercizio01/thomas/esercizio_23122022.py
@@ -0,0 +1,38 @@
+#Stampare a terminale per cento volte il vostro nome e il vostro cognome
+#es 1
+'''
+nome=input('inserisci il tuo nome e cognome:')
+
+def ripetitore():
+    for i in range(100):
+        print(nome)
+
+print(ripetitore())
+'''
+
+
+
+#es2
+#Stampare a terminale tutti i numeri interi da 1 a 20
+'''
+def stampinteri():
+    x=0
+    while x<20:
+        x+=1
+        print(x)
+print (stampinteri())
+'''
+
+#es3
+#Stampare a terminale tutti i numeri pari da 1 a 20 e dopo tutti i numeri dispari da 1 a 20
+for d in range(3, 7, 2):
+        print(d)
+        '''
+def stampdipa(c):
+
+    a= range(1+c, 21+c,2)
+    for b in a:
+        print(b)
+
+print(stampdipa())
+'''
