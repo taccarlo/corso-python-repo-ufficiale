aee99ad1de998fc945ffb11895a85369ab4c76f2
Tscippo02 <gabrielesegala02@gmail.com>
2022-12-23 15:43:06 +0100

    Update testo_esercizi.py

--- lezione06/esercizio01/testo_esercizi.py
+++ lezione06/esercizio01/testo_esercizi.py
@@ -10,45 +10,5 @@ Chiedere all'utente un numero da inserire, se questo è divisibile per 3 stampa 
 
 Creare un programma che continua a chiedere all'utente una password finché l'utente non la indovina
 
-1
-for i in range (100):
-    i+=1
-    print("Gabriele Segala")
-
-2
-for i in range (20):
-    i+=1
-    print(i)
-
-3
-i=0
-while(i<20):
-    i=i+2
-    print(i)
-
-i=1
-while(i<20):
-    print(i)
-    i=i+2
-
-4
-num = int(input("inserici un numero: "))
-
-if num%3==0:
-    for i in range (10):
-        print("Gabriele Segala")
-        i+=1
-else:
-    for i in range (10):
-        print("non è divisibile per 3")
-        i+=1
-
-
-
-password = "ciaoney"
-str =input("Enter your password :")
-while password!=str:
-    print ("password non valida" )
-    str =input("Enter your password :")
 
 '''
