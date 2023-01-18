40a1f4fd0d017a9fa20bfdf8b90e452bfd5904b1
SimoneLanza97 <119517246+SimoneLanza97@users.noreply.github.com>
2023-01-18 16:33:23 +0100

    .

---
+++ lezione07/esercizio03/lanzarottosimone/esercizio03.py
@@ -0,0 +1,21 @@
+registro = {"paolo":[] , "antonio":[], "giorgio":[]}
+print("Benvenuto nel registro!")
+print("1 -> aggiungi studente , 2 ->rimuovi studente, 3-> visualizza registro, 4-> esci ")
+def chiedi_azione():
+    azione=int(input("inserisci un azione: "))
+    if azione == 1:
+        aggiungi_studente()
+    elif azione == 2:
+        rimuovi_studente()
+    elif azione == 3:
+        visualizza_registro()
+    elif azione == 4:
+        
+
+def aggiungi_studente():
+    return
+def rimuovi_studente():
+    return
+def visualizza_registro():
+
+    return
