0b8ed459aaee373a8ea464d75884ba477551fa9b
N1M3Xx <119055821+N1M3Xx@users.noreply.github.com>
2023-01-18 15:17:26 +0100

    Update es3.py

--- lezione07/esercizio01/gonzalo_perezbrisco/es3.py
+++ lezione07/esercizio01/gonzalo_perezbrisco/es3.py
@@ -1,17 +1,8 @@
 def controllo(a):
-  min = a[0]
-  max = a[0]
-  media = a[0]
   for i in range(1, len(a)):
     if not type(a[i]) == int:
       return "error"
-    else:
-      if a[i] < min:
-        min = a[i]
-      if a[i] > max:
-        max = a[i]
-      media += a[i]
       
-  return ([min, media/len(a), max])
+  return ([min(a), sum(a)/len(a), max(a)])
   
 print(controllo([1, 2, 3, 4, 5]))
