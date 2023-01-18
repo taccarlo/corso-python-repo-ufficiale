9178faffb516e5120d832b642eaff4ed849523cf
SimoneLanza97 <119517246+SimoneLanza97@users.noreply.github.com>
2023-01-18 15:27:02 +0100

    .
    .

--- lezione07/esercizio01/esercizio_lez_7.py
+++ lezione07/esercizio01/esercizio_lez_7.py
@@ -24,3 +24,38 @@ def string(x):
     return frase
 
 print (string(list02))
+
+#es3
+
+lista_3=[11,48,23,55,44,33]
+
+
+
+def funz_3(lista_3):
+    x = 1 #variabile intera per controllare la classe degli elemnti della lista
+    copia=[]
+    min=[]
+    magg=[]
+    i=0
+    somma=0
+    while i<len(lista_3):
+        if type(lista_3[i]) != type(x):
+            print("Error")
+        else:
+            somma+=lista_3[i]
+        i+=1
+    media=somma/i
+    
+    minimo=1000000000000
+    for x in range (len(lista_3)):
+        if lista_3[x]<minimo:
+            minimo=lista_3[x]
+    media=sum(lista_3)/len(lista_3)
+    massimo=0
+    for x in range (len(lista_3)):
+        if lista_3[x]>massimo:
+            massimo=lista_3[x]
+    newlist=[minimo,media,massimo,]
+    return newlist
+
+print(funz_3(lista_3))
