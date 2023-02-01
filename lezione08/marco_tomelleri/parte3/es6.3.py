dbdfad9609c41af5d4dc62ea96457f47c0d3a4bd
tome <tometom0120@gmail.com>
2023-02-01 17:08:19 +0100

    rinominato da 6.3 a 7

---
+++ lezione08/marco_tomelleri/parte3/es6.3.py
@@ -0,0 +1,2 @@
+'''Facendo riferimento all’esercizio 6 potete trovare molti giochi basati sul semplice lancio di dadi come ad esempio Yahtzee o Poker (coi dadi). Provate, se avete tempo e voglia, a realizzare una versione (anche minimale) di questi giochi'''
+
